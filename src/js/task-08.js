84cf56097d2628b5fc45807ad0c294c812b85074
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-04 15:58:14 +0300

    done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,7 +1,27 @@
 /**
  * Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
 Під час відправлення форми сторінка не повинна перезавантажуватися.
+
 Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
  */
+
+const loginForm = document.querySelector(".login-form");
+const data = {};
+
+loginForm.addEventListener("submit", onLoginFormSubmit);
+
+function onLoginFormSubmit(e) {
+  e.preventDefault();
+  const emailValue = e.target.elements.email.value.trim();
+  const passwordValue = e.target.elements.password.value.trim();
+
+  (!emailValue || !passwordValue) &&
+    alert("Введіть валідні дані в обидва поля!");
+
+  data.email = emailValue;
+  data.password = passwordValue;
+  console.log(data);
+  loginForm.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,34 @@
+/**
+ * Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+
+Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+
+ */
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  colorName: document.querySelector(".color"),
+};
+
+refs.changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+
+function onChangeColorBtnClick(e) {
+  e.preventDefault();
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+
+  refs.colorName.textContent = randomColor;
 }
